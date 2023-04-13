7a04f5b9f32848f344bccb4e03b58a3989597f08
JoséRafaell <rs409114@gmail.com>
2023-04-13 08:25:59 -0300

    Criado laço while

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.WeakHashMap;
+
 public class Main {
     public static void main(String[] args) {
         int contador = 0;
@@ -11,6 +13,13 @@ public class Main {
         for(int x = 0; x < 10; x++) {
             System.out.println("Contador = " + x);
         }
-
+        // Laço faça enquanto
+        int cont = 1;
+        do {
+            //Processo
+            System.out.println(cont);
+            //Incremento
+            cont++;
+        }while(cont <= 10);
     }
 }
