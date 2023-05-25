218072055395f33d02b6abf87039937cd27ada7a
rm87476 <rm87476@estudante.fieb.edu.br>
2023-05-25 09:48:55 -0300

    Aula dia 18/05/23, classe é um modelo.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
-import java.util.WeakHashMap;
 
 public class Main {
     public static void main(String[] args) {
-       //Aula Introdutoria de Orientação Objetos
-    // Declaração de Objetos
-    Pessoa adao;
-    //Instanciação de objeto
-    adao = new Pessoa();
-    //Ao mesmo tempo
-    Pessoa qualquer = new Pessoa();
-    //Definir formato do objeto
-    qualquer.nome = "Rita";
-    qualquer.sobrenome = "Lee";
-    qualquer.falar();
-        System.out.println((qualquer.falar("oi"));
+
+        //Aula Introdutoria de Orientação Objetos
+        // Declaração de Objetos
+        Pessoa adao;
+
+        //Instanciação de objeto
+        adao = new Pessoa();
+
+        //Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+
+        //Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
     }
 }
