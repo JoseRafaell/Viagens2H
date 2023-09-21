9f027b54371f6dc90e114c49144da3fcbafd7c84
José Rafael de Almeida Soares <rm87476@estudante.fieb.edu.br>
2023-09-21 10:56:43 -0300

    revisao de conceitos e entrada dinamica

--- Viagens2H/src/Main.java
+++ Viagens2H/src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,7 +12,21 @@ public class Main {
         Turista mochileiro = new Turista("João");
 
         //Definir comportameto
-       String retorno = mochileira.viajar();
+        String retorno = mochileira.viajar();
         System.out.println(retorno);
+
+        //Criar um novo objeto
+        Turista outroTurista = new Turista();
+        String nome = JOptionPane.showInputDialog("Entre com seu nome:");
+        outroTurista.setNome(nome);
+        JOptionPane.showInputDialog(null, "O nome digitado foi " + outroTurista.getNome());
+
+        //Criar a captura do CPF deste objeto
+        String cpf = JOptionPane.showInputDialog("Entre com seu cpf:");
+        if (Validacao.cpf(cpf))
+            outroTurista.setCpf(cpf);
+         else
+            System.out.println("ERRO!!! CPF NÃO VALIDADO");
+            JOptionPane.showInputDialog(null, outroTurista.setCpf() + "Validado!");
+        }
     }
-}
